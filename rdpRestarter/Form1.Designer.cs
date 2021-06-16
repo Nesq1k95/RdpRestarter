8cd68bb1300fe9dd23d6000d77526c8e7f3ea37c
a.simonov <a.simonov@npo-pkrv.ru>
2021-06-16 15:47:28 +0300

    master: Работает!

---
+++ rdpRestarter/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace rdpRestarter
+{
+    partial class RdpRestarter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RdpRestarter));
+            this.text1 = new System.Windows.Forms.Label();
+            this.yesButton = new System.Windows.Forms.Button();
+            this.HourButton = new System.Windows.Forms.Button();
+            this.dayButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.testButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // text1
+            // 
+            resources.ApplyResources(this.text1, "text1");
+            this.text1.Name = "text1";
+            // 
+            // yesButton
+            // 
+            resources.ApplyResources(this.yesButton, "yesButton");
+            this.yesButton.Name = "yesButton";
+            this.yesButton.UseVisualStyleBackColor = true;
+            this.yesButton.Click += new System.EventHandler(this.yesButton_Click);
+            // 
+            // HourButton
+            // 
+            resources.ApplyResources(this.HourButton, "HourButton");
+            this.HourButton.Name = "HourButton";
+            this.HourButton.UseVisualStyleBackColor = true;
+            this.HourButton.Click += new System.EventHandler(this.HourButton_Click);
+            // 
+            // dayButton
+            // 
+            this.dayButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.dayButton, "dayButton");
+            this.dayButton.Name = "dayButton";
+            this.dayButton.UseVisualStyleBackColor = true;
+            this.dayButton.Click += new System.EventHandler(this.dayButton_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timeLabel
+            // 
+            resources.ApplyResources(this.timeLabel, "timeLabel");
+            this.timeLabel.Name = "timeLabel";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // testButton
+            // 
+            resources.ApplyResources(this.testButton, "testButton");
+            this.testButton.Name = "testButton";
+            this.testButton.UseVisualStyleBackColor = true;
+            this.testButton.Click += new System.EventHandler(this.testButton_Click);
+            // 
+            // RdpRestarter
+            // 
+            this.AcceptButton = this.yesButton;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.dayButton;
+            this.Controls.Add(this.testButton);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dayButton);
+            this.Controls.Add(this.HourButton);
+            this.Controls.Add(this.yesButton);
+            this.Controls.Add(this.text1);
+            this.Name = "RdpRestarter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label text1;
+        private System.Windows.Forms.Button yesButton;
+        private System.Windows.Forms.Button HourButton;
+        private System.Windows.Forms.Button dayButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label timeLabel;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button testButton;
+    }
+}
+
---
+++ rdpRestarter/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+using System.Windows.Forms;
+
+namespace rdpRestarter
+{
+    public partial class RdpRestarter : Form
+    {
+        int hour = 1000 * 60 * 60 * 1;
+        int day = 1000 * 60 * 60 * 24;
+        bool stopCountdown = false;
+
+
+        public RdpRestarter()
+        {
+            InitializeComponent();
+            testButton.Visible = false;
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+            backgroundWorker1.RunWorkerAsync();
+
+            timer1.Stop();
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!this.Visible || this.WindowState == FormWindowState.Minimized)
+                this.Show();
+
+            backgroundWorker1.RunWorkerAsync();
+
+            timer1.Stop();
+        }
+
+        private void dayButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            timer1.Interval = day;
+            timer1.Start();
+            stopCountdown = true;
+          
+            backgroundWorker1.CancelAsync();      // Cancel the asynchronous operation.
+        }
+
+        private void HourButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            timer1.Interval = hour;
+            timer1.Start();
+            stopCountdown = true;
+
+            backgroundWorker1.CancelAsync();      // Cancel the asynchronous operation.
+        }
+
+        private void yesButton_Click(object sender, EventArgs e)
+        {
+            restartRdp();
+            stopCountdown = true;
+            timer1.Interval = hour;
+            timer1.Start();
+            Hide();
+        }
+
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            for (int i = 10; i != -1; --i)
+            {
+                Thread.Sleep(1000);
+                this.Invoke((MethodInvoker)delegate
+                {
+                    timeLabel.Text = i.ToString();
+                });
+
+                if (i == 0 && !stopCountdown)
+                {
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        restartRdp();
+                        timer1.Interval = hour;
+                        timer1.Start();
+                        Hide();
+                    });
+                }
+            }
+        }
+
+        private void testButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            timer1.Interval = 1000 * 10;
+            timer1.Start();
+        }
+
+        private void restartRdp()
+        {
+            var SERVICENAME1 = "UmRdpService";
+            var SERVICENAME2 = "TermService";
+
+            restartService(SERVICENAME1);
+            restartService(SERVICENAME2);
+        }
+
+        void restartService(string serviceName)
+        {
+            try
+            {
+                var sc = new ServiceController(serviceName);
+
+
+                if (sc.Status != ServiceControllerStatus.Stopped)
+                {
+                    sc.Stop();
+
+                    while (sc.Status != ServiceControllerStatus.Stopped)
+                    {
+                        Thread.Sleep(1000);
+                        sc.Refresh();
+                    }
+                }
+
+                sc.Start();
+
+                while (sc.Status == ServiceControllerStatus.Stopped)
+                {
+                    Thread.Sleep(1000);
+                    sc.Refresh();
+                }
+
+                sc.Dispose();
+                //MessageBox.Show("Cлужба " + serviceName + "перезапущена");
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось перезапустить службу " + serviceName);
+            };
+        }
+    }
+}
